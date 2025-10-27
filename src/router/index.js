c305e7bc48733890a3c021473a17c8182a2c7ed5
hspacesi34 <107484761+hspacesi34@users.noreply.github.com>
2025-10-27 14:58:20 +0100

    feat: ajout routes children

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 const routes = [
   { path: '/', name:'home', component: () => import('../views/Home.vue') },
   { path: '/contact', name:'contact', component: () => import('../views/Contact.vue') },
-  { path: '/lessons/text-interpolation', component: () => import('../views/lessons/TextInterpolation.vue') },
-  { path: '/exos/text-interpolation', component: () => import('../views/exos/TextInterpolation.vue') }
+  { path: '/lessons', children: [
+    { path: 'text-interpolation', component: () => import('../views/lessons/TextInterpolation.vue') }
+  ] },
+  { path: '/exos', children: [
+    { path: 'text-interpolation', component: () => import('../views/exos/TextInterpolation.vue') }
+  ] }
 ]
 
 const router = createRouter({
