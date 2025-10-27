a6ac411dc904941c0595390f7362fe73be2bf0cc
hspacesi34 <107484761+hspacesi34@users.noreply.github.com>
2025-10-27 10:58:37 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+function counter(e) {
+  count.value++;
+  console.log(e);
+}
+</script>
+
+<template>
+  <h1 class="mb-4 mt-4">{{ msg }}</h1>
+  <div>
+    <button class="btn btn-primary w-38" type="button" @click="(e) => counter(e)">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
